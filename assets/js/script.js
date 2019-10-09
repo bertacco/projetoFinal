b973b21fec1182d442a14e9c6da78bb2535fb097
bertacco <bertaccode@gmail.com>
2019-10-08 21:56:07 -0300

    inserido cards de maltes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,51 @@
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+	  slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+	}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
+
+setInterval (function(){
+	plusSlides(1);
+	
+},3000);
+
+
+var botaomalte = document.getElementById('botaomalte');
+var carrossel = document.getElementById('carrossel');
+var maltes = document.getElementById('maltes');
+
+botaomalte.addEventListener ('click', function() {
+	carrossel.classList.add("desligado"); 
+	maltes.classList.remove("desligado"); 
+	/* toggle add ou remove a classe */
+})
+
+
+
 /*
 var tarefa = document.getElementById('tarefa');
 var listaTarefa = document.getElementById('lista-tarefas');
